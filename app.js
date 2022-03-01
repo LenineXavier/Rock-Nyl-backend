79a1910ab3502304c5c31f71754789755773a431
Bruno Filippini <brunofilippini94@gmail.com>
2022-03-01 18:21:13 -0300

    Crud account Done

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.use(morgan("dev"));
 // Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
 app.use(cors({ origin: process.env.REACT_APP_URL }));
 
-const userRouter = require("./routes/user.routes");
-app.use("/api", userRouter);
+const accountRouter = require("./routes/user.routes");
+app.use("/account", accountRouter);
 
 app.listen(Number(process.env.PORT), () =>
   console.log(`Server up and running at port ${process.env.PORT}`)
