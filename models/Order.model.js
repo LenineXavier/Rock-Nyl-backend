2ea0944da53f908f43849d53a1d5fae23fd62113
Bruno Filippini <brunofilippini94@gmail.com>
2022-03-07 15:37:49 -0300

    update schemma

--- models/Order.model.js
+++ models/Order.model.js
@@ -1,14 +1,14 @@
-const  mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 const { Schema, model } = require("mongoose");
 
 const orderSchema = new Schema({
-  product: {type: mongoose.Schema.Types.ObjectId, ref:"Product"},
+  product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
   quantityPurchased: { type: Number, required: true, default: 1 },
   totalAmount: { type: Number },
   date: { type: Date, default: Date.now },
   isDeleted: { type: Boolean, default: false },
   deletedDate: { type: Date },
-  productsOrderList: [{ type: mongoose.Schema.Types.ObjectId, ref:"Product"}],
+  productsOrderList: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
 });
 
 const OrderModel = model("Order", orderSchema);
